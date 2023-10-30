7932f4055a5e92d35f8466f6c6f712d0aa019907
Chris Pawley <christopher.pawley@gmail.com>
2023-10-30 13:04:08 +0100

    Updates jest

--- test/utils/mockAxios.ts
+++ test/utils/mockAxios.ts
@@ -1,7 +1,7 @@
 import { object, replace, when } from 'testdouble';
 
 export const mockAxios = (): { axios: { get: any; post: any; defaults: any } } => {
-  const axios = replace('axios');
+  const axios = replace('axios') as any;
   const axiosInstance = object(['get', 'post', 'defaults']);
 
   when(axios.create(), { ignoreExtraArgs: true }).thenReturn(axiosInstance);
