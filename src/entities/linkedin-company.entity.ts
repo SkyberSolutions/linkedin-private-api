de183be59ef36c76daba5c3f1d789927e202b980
Chris Pawley <christopher.pawley@gmail.com>
2023-11-10 11:18:35 +0100

    Updates to company entity

--- src/entities/linkedin-company.entity.ts
+++ src/entities/linkedin-company.entity.ts
@@ -1,17 +1,19 @@
+import { LinkedInIntegerRange } from './linkedin-integer-range.entity';
 import { LinkedInVectorImage } from './linkedin-vector-image.entity';
 
 export const COMPANY_TYPE = 'com.linkedin.voyager.dash.organization.Company';
 
 export interface LinkedInCompany {
   $type: typeof COMPANY_TYPE;
-  $anti_abuse_annotations: {
+  $anti_abuse_annotations?: {
     attributeId: number;
     entityId: number;
   }[];
   $recipeTypes: string[];
   entityUrn: string;
-  industry: Record<string, string>;
-  industryUrns: string[];
+  employeeCountRange?: LinkedInIntegerRange;
+  industry?: Record<string, string>;
+  industryUrns?: string[];
   logo: {
     vetorImage: LinkedInVectorImage;
   };
