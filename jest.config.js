2ec98072e8161cb56f5f1559a84c76394b26a8b7
Chris Pawley <christopher.pawley@gmail.com>
2024-12-19 17:04:28 +0100

    Fix jest tests with moduleNameMapper

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
+  // REFERENCE: https://stackoverflow.com/questions/73958968/cannot-use-import-statement-outside-a-module-with-axios
+  moduleNameMapper: {
+    "axios": require.resolve("axios"),
+  },
   testEnvironment: 'node',
   testMatch: ['<rootDir>/test/**/*.spec.ts'],
   setupFilesAfterEnv: ['<rootDir>/test/setup.ts'],
